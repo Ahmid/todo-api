3b0a7a27dc1278dcda07d5e308b3ab002ebd659d
Ahmad Hijazi <ahmad-hjz@hotmail.com>
2017-02-09 16:29:14 +0200

    Added post

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express = require ('express');
+var bodyParser = require ('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3005;
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-}, {  
-    id:2,
-    description: 'Go to market',
-    completed: false
-}];
+var todos = [];
+var todoNextID = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send ('Todo API Root');
@@ -39,6 +36,20 @@ app.get ('/todos/:id', function (req, res) {
     }
 
     res.send ('Asking for todo with id of ' + req.params.id);
+});
+
+// POST /todos
+app.post ('/todos', function (req, res) {
+    var body = req.body;
+
+    //add id to body
+    body.id = todoNextID;
+    todoNextID++;
+    
+    //push body into todo array
+    todos.push(body);
+
+    res.json(body);
 });
 
 app.listen(PORT, function () {
