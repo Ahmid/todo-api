fa49ca9b26899bb7f1760c95afde25e31be8cc2c
Ahmad Hijazi <ahmad-hjz@hotmail.com>
2017-02-20 20:41:15 +0200

    Add auth middleware and and findByToken class method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require ('body-parser');
 var _ = require ('underscore');
 var bcrypt = require ('bcryptjs');
 var db = require ('./db.js');
+var middleware = require ('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3005;
@@ -16,7 +17,7 @@ app.get('/', function (req, res) {
 });
 
 //GET /todos?completed=true&q=house
-app.get ('/todos', function (req, res) {
+app.get ('/todos', middleware.requireAuthentication, function (req, res) {
     var query = req.query;
     var where = {};
 
@@ -57,7 +58,7 @@ app.get ('/todos', function (req, res) {
 });
 
 //GET /todos/:id
-app.get ('/todos/:id', function (req, res) {    
+app.get ('/todos/:id', middleware.requireAuthentication, function (req, res) {    
     var todoID = parseInt(req.params.id,10);
     
     db.todo.findById(todoID).then (function (todo) {
@@ -82,7 +83,7 @@ app.get ('/todos/:id', function (req, res) {
 });
 
 //POST /todos
-app.post ('/todos', function (req, res) {
+app.post ('/todos',middleware.requireAuthentication, function (req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
     db.todo.create(body).then (function (todo) {
@@ -108,7 +109,7 @@ app.post ('/todos', function (req, res) {
 });
 
 //Delete /todos/:id
-app.delete ('/todos/:id', function (req, res) {
+app.delete ('/todos/:id', middleware.requireAuthentication, function (req, res) {
     var todoId = parseInt (req.params.id, 10);
     db.todo.destroy ({
         where: {
@@ -138,7 +139,7 @@ app.delete ('/todos/:id', function (req, res) {
 });
 
 //Update /todos:id
-app.put ('/todos/:id', function (req , res) {
+app.put ('/todos/:id',middleware.requireAuthentication, function (req , res) {
     var todoId = parseInt (req.params.id, 10);
     var body =  _.pick(req.body, 'description', 'completed');
     var attributes = {};
@@ -207,11 +208,13 @@ app.post('/users/login', function (req, res) {
         var token = user.generateToken('authentication');
         if (token) {
             res.header('Auth', token).json(user.toPublicJSON());
+        } else {
+            res.status(401).send();
         }
     }, function (e) {
         res.status(401).send();
     });
-
+    
 });
 
 
